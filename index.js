5e92b08207d08630d352fee28e290a733ac4378f
diman19 <yadiman19@gmail.com>
2021-09-15 03:51:23 +0700

    calculator created

---
+++ index.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  $('#1,#2,#3,#4,#5,#6,#7,#8,#9,#0,#add, #subtract, #multiply, #divide, #power, #dot, #para1, #para2').click(function () {
+    let v = $(this).val();
+    let total = $('textarea').val($('textarea').val() + v);
+  });
+
+  $('#equal').click(function () {
+    $('textarea').val(eval($('textarea').val()));
+  });
+
+
+  $('#clear').click(function () {
+    $('textarea').val('');
+  });
+
+
+  $('#backspace').click(function () {
+    $('textarea').val($('textarea').val().substring(0, $('textarea').val().length - 1));
+  });
+});
